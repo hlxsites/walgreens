998ab0b5f3bd8dfaf13268c3b891ddfb1405d9f6
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-09-13 21:21:00 +0200

    13 - Cards Block (#24)

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,14 +1,29 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+  const cardsWithBorder = block.classList.contains('border');
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    li.classList.add('card');
+    if (cardsWithBorder) {
+      li.classList.add('with-border');
+    }
+
+    const link = row.querySelector('a');
+    let parent = li;
+    if (link) {
+      link.textContent = '';
+      parent = link;
+      li.appendChild(link);
+    }
+
+    while (row.firstElementChild) parent.append(row.firstElementChild);
+    [...parent.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-image';
+      else div.className = 'card-body';
     });
     ul.append(li);
   });
