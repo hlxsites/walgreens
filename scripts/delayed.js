bd1d0b0bdc94e7bc7e0877358fdaf698cd2cb04e
Rob Rusher <robrusher@gmail.com>
2023-09-12 18:58:08 -0600

    add script to delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,22 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+const loadScript = (url, attrs) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
+    }
+  }
+  head.append(script);
+  return script;
+};
+
+loadScript('https://resources.digital-cloud-west.medallia.com/wdcwest/378975/onsite/embed.js', {
+  type: 'text/javascript',
+  async: true,
+});
