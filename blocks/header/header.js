7482fc82fcbf2edb6a83f14ee08965e91fa56b86
Rob Rusher <robrusher@gmail.com>
2023-09-14 10:56:44 -0600

    linting

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -123,19 +123,6 @@ export default async function decorate(block) {
       });
     }
 
-    // hamburger for mobile
-    /* const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-        <span class="nav-hamburger-icon"></span>
-      </button>`;
-    hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-    nav.prepend(hamburger);
-    nav.setAttribute('aria-expanded', 'false'); */
-    // prevent mobile nav behavior on window resize
-    /* toggleMenu(nav, navSections, isDesktop.matches);
-    isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches)); */
-
     decorateIcons(nav);
     const navWrapper = document.createElement('div');
     navWrapper.className = 'nav-wrapper';
