9a9cb919b02bac9d1fe348809d8e886b69a89db3
Laith Alwani <laitha@adobe.com>
2023-09-21 12:56:31 -0400

    fixes lint errors

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,16 +1,12 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
-
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch('https://www.walgreens.com/common/v1/footerui', window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  const resp = await fetch('https://www.walgreens.com/common/v1/footerui');
 
   if (resp.ok) {
     const data = await resp.json();
@@ -20,7 +16,7 @@ export default async function decorate(block) {
     decorateIcons(footer);
     block.append(footer);
 
-    const footerStyles = document.createElement("style");
+    const footerStyles = document.createElement('style');
     footerStyles.innerHTML = data.clientLSGCSSContent;
     document.head.appendChild(footerStyles);
   }
