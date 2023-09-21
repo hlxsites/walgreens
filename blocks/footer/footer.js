fbc2dc5c459fcef766f26a08749ae21e71eb7604
Laith Alwani <laitha@adobe.com>
2023-09-21 12:15:33 -0400

    fetchs footer from api

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,16 +10,18 @@ export default async function decorate(block) {
 
   // fetch footer content
   const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  const resp = await fetch('https://www.walgreens.com/common/v1/footerui', window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
-    const html = await resp.text();
-
+    const data = await resp.json();
     // decorate footer DOM
     const footer = document.createElement('div');
-    footer.innerHTML = html;
-
+    footer.innerHTML = data.content;
     decorateIcons(footer);
     block.append(footer);
+
+    const footerStyles = document.createElement("style");
+    footerStyles.innerHTML = data.clientLSGCSSContent;
+    document.head.appendChild(footerStyles);
   }
 }
