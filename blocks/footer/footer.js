0791b5d34a940b564dcc036ddc35048b50fb5239
Laith Alwani <laitha@adobe.com>
2023-09-21 13:25:51 -0400

    adds footer ui via API, deletes footer.css

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,25 +1,20 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
-
+import { decorateIcons } from '../../scripts/lib-franklin.js';
 /**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
+* loads and decorates the footer
+* @param {Element} block The footer block element
+*/
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
-
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
-
+  const resp = await fetch('https://www.walgreens.com/common/v1/footerui');
   if (resp.ok) {
-    const html = await resp.text();
-
+    const data = await resp.json();
     // decorate footer DOM
     const footer = document.createElement('div');
-    footer.innerHTML = html;
-
+    footer.innerHTML = data.content;
     decorateIcons(footer);
     block.append(footer);
+    const footerStyles = document.createElement('style');
+    footerStyles.innerHTML = data.clientLSGCSSContent;
+    document.head.appendChild(footerStyles);
   }
 }
