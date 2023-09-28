cc91d2861540eafacdc1117301ec3a6d84665a17
Laith Alwani <laitha@adobe.com>
2023-09-28 12:18:33 -0400

    fixes hero image issue

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,13 @@
 export default function decorate(block) {
   const link = block.querySelector('a');
+  const picture = block.querySelector('picture');
+
+  if (picture) {
+    block.prepend(picture);
+  }
+
   if (link) {
     link.textContent = '';
+    block.prepend(link);
   }
 }
