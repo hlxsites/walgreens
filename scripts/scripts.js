955538fde4540c3fab4fa79a0e49362ffcd058c2
Rob Rusher <robrusher@gmail.com>
2023-09-29 11:31:03 -0600

    removed spacing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -198,7 +198,7 @@ async function loadLazy(doc) {
     loadHeader(doc.querySelector('header'));
     loadFooter(doc.querySelector('footer'));
   }
-  
+
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
 
