41561f75c661b402aca43000bf4f13be52eaf8f4
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-09-29 18:12:35 +0200

    9 - FAQ Accordion

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,61 +1,44 @@
+import { div, span } from '../../scripts/dom-helpers.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
 function closeAllOtherFaqs(faq) {
-    document.querySelectorAll('.faq-accordion').forEach((acc) => {
-      if (acc !== faq && acc.classList.contains('active')) {
-        acc.classList.remove('active');
-      }
-    });
-  }
-  
-  function toggleFaq(e) {
-    const faq = e.target.parentElement;
-    closeAllOtherFaqs(faq);
-    faq.classList.toggle('active');
-  }
-  
-  function addFaqEventListeners(block) {
-    block.querySelectorAll('.faq-question').forEach((question) => {
-      question.addEventListener('click', toggleFaq);
-    });
-  }
-  
-  export default function decorate(block) {
-    const faqs = [];
-    const rows = Array.from(block.children);
-    rows.forEach((row) => {
-      const cells = Array.from(row.children);
-      const question = cells[0] && cells[0].textContent;
-      const answer = cells[1] && cells[1].innerHTML;
-      faqs.push({
-        question, answer,
-      });
-    });
-  
-    block.innerHTML = '';
-    faqs.forEach((faq, i) => {
-      const { question, answer } = faq;
-  
-      const accordion = document.createElement('div');
-      accordion.className = 'faq-accordion';
-      block.append(accordion);
-  
-      const questionDiv = document.createElement('div');
-      questionDiv.className = 'faq-question';
-      accordion.append(questionDiv);
-      questionDiv.innerHTML = question;
-  
-      const chevron = document.createElement('i');
-      chevron.className = 'chevron';
-      questionDiv.append(chevron);
-  
-      const answerDiv = document.createElement('div');
-      answerDiv.className = 'faq-answer';
-      accordion.append(answerDiv);
-      answerDiv.innerHTML = answer;
-  
-      if (i === 0) {
-        accordion.classList.add('active');
-      }
-    });
-  
-    addFaqEventListeners(block);
-  }
+  faq.querySelectorAll('.faq-accordion').forEach((acc) => {
+    if (acc !== faq && acc.classList.contains('active')) {
+      acc.classList.remove('active');
+    }
+  });
+}
+
+function toggleFaq(e) {
+  const faq = e.target.parentElement;
+  closeAllOtherFaqs(faq);
+  faq.classList.toggle('active');
+}
+
+export default async function decorate(block) {
+  const faqs = [];
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    const question = cells[0] && cells[0].textContent;
+    const answer = cells[1] || div();
+    answer.classList.add('faq-answer');
+    faqs.push({ question, answer });
+  });
+
+  block.innerHTML = '';
+  faqs.forEach((faq, i) => {
+    const { question, answer } = faq;
+
+    block.append(
+      div({ class: 'faq-accordion' },
+        div({ class: 'faq-question', onclick: toggleFaq },
+          question,
+          span({ class: 'icon icon-arrow-down' }),
+        ),
+        answer,
+      ),
+    );
+  });
+
+  await decorateIcons(block);
+}
