26a13f6acfc76c0e15a226b72369448e3f6c05e7
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-02 11:55:25 +0200

    25 - Breadcrumb

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -10,12 +10,16 @@ export default async function decorate(block) {
   const mobileSection = (
     div({ class: 'section breadcrumb-container', 'data-section-status': 'loading' },
       div({ class: 'breadcrumb-wrapper' },
-        div({ class: 'breadcrumb block mobile', 'data-block-name': 'breadcrumb', 'data-block-status': 'loaded' }),
+        div({
+          class: 'breadcrumb block mobile',
+          style: 'display: none;', // hide in the beginning to avoid CLS
+          'data-block-name': 'breadcrumb',
+          'data-block-status': 'loaded',
+        }),
       ),
     )
   );
   const mobileBlock = mobileSection.querySelector('.block');
-  mobileBlock.style.display = 'none'; // hide in the beginning to avoid CLS
   mobileBlock.append(desktopBlock.children[0].cloneNode(true));
 
   const main = block.closest('main');
