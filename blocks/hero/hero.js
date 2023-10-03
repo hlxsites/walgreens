395613318495d25e34087ef18ea54a2acc8f0b3b
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-03 18:05:31 +0200

    7 - Hero Block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,13 +1,30 @@
 export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    if (row.children.length < 2) return;
+
+    const configCell = row.children[0];
+
+    if (configCell.textContent.toLowerCase().trim() === 'desktop') {
+      row.classList.add('background-desktop');
+    }
+
+    if (configCell.textContent.toLowerCase().trim() === 'mobile') {
+      row.classList.add('background-mobile');
+    }
+
+    if (configCell.textContent.toLowerCase().trim() === 'overlay') {
+      row.classList.add('hero-overlay');
+    }
+
+    configCell.remove();
+  });
+
   const link = block.querySelector('a');
-  const picture = block.querySelector('picture');
+  link.parentElement.remove();
 
-  if (picture) {
-    block.prepend(picture);
-  }
-
-  if (link) {
+  if (link) {    
     link.textContent = '';
-    block.prepend(link);
+    link.append(...block.children);
+    block.append(link);
   }
 }
