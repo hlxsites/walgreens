0d18a01e5d60a572bedf20af659a5a299591f98c
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-04 18:12:02 +0200

    Revert "test - TBT"
    This reverts commit 76796fde26973e1cc7ecec3148f62dfcdb2a3ccb.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,13 +1,14 @@
 import { loadCSS } from '../../scripts/lib-franklin.js';
 import { loadFileList } from '../../scripts/scripts.js';
 
-async function addContent(block, jsonData) {
+async function addContent(block, jsonData, cssPromises) {
   const nav = document.createElement('nav');
   nav.id = 'nav';
   nav.innerHTML = jsonData.content;
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
+  await Promise.all(cssPromises);
   block.firstElementChild.replaceWith(navWrapper);
 }
 
@@ -16,7 +17,12 @@ async function addContent(block, jsonData) {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
+   const cssPromises = [
+     loadCSS(`${window.hlx.codeBasePath}/external-styles/header-clientCSSContent.css`),
+     loadCSS(`${window.hlx.codeBasePath}/external-styles/header-clientLSGCSSContent.css`),
+  ];
   const worker = new Worker('../../scripts/headerfooter-worker.js');
+
   worker.onmessage = async (e) => {
     worker.terminate();
 
@@ -24,7 +30,7 @@ export default async function decorate(block) {
       return;
     }
     const jsonData = e.data;
-    addContent(block, jsonData);
+    addContent(block, jsonData, cssPromises);
     loadFileList(jsonData.fileList);
   };
   worker.postMessage({ source: 'header' });
