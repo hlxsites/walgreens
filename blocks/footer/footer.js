068debae7bd6a33ce8fd32467dad370e41e15189
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-04 10:33:39 +0200

    test - CLS 5

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,17 +1,17 @@
-import { decorateIcons } from '../../scripts/lib-franklin.js';
-import { resolveRelativeURLs } from '../../scripts/scripts.js';
+import { decorateIcons } from "../../scripts/lib-franklin.js";
+import { resolveRelativeURLs } from "../../scripts/scripts.js";
 
 /**
-* loads and decorates the footer
-* @param {Element} block The footer block element
-*/
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
 export default async function decorate(block) {
   // fetch footer content
-  const resp = await fetch('https://www.walgreens.com/common/v1/footerui');
+  const resp = await fetch("https://www.walgreens.com/common/v1/footerui");
   if (resp.ok) {
     const data = await resp.json();
     // decorate footer DOM
-    const footer = document.createElement('div');
+    const footer = document.createElement("div");
     footer.innerHTML = resolveRelativeURLs(data.content);
     decorateIcons(footer);
     block.append(footer);
