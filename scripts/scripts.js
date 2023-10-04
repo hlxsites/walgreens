e97a958476c3173f4a7e1a3964bd2b044134be4e
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-04 18:03:35 +0200

    test - TBT

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -169,6 +169,11 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  const noHeader = new URLSearchParams(window.location.search).has('test');
+  if (!noHeader) {
+    loadHeader(doc.querySelector('header'));
+  }
+
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
@@ -177,9 +182,7 @@ async function loadLazy(doc) {
   if (hash && element) element.scrollIntoView();
 
   // TODO: remove this check before go-live
-  const noHeader = new URLSearchParams(window.location.search).has('test');
   if (!noHeader) {
-    loadHeader(doc.querySelector('header'));
     loadFooter(doc.querySelector('footer'));
   }
 
