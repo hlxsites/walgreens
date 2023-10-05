3f3f4f6acef3d2a3cf884f0bfb67a03248d4b33a
mtemk <mtemkit@adobe.com>
2023-10-05 15:52:13 -0400

    Fixing Animation for Display

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -5,6 +5,7 @@ function closeAllOtherFaqs(faq) {
   const allFaqs = document.querySelectorAll('.faq-accordion');
   allFaqs.forEach((acc) => {
     if (acc !== faq && acc.classList.contains('active')) {
+      acc.querySelector('.faq-answer').style.maxHeight = '0';
       acc.classList.remove('active');
     }
   });
@@ -12,12 +13,23 @@ function closeAllOtherFaqs(faq) {
 
 function toggleFaq(e) {
   const faq = e.target.closest('.faq-accordion');
+  const answer = faq.querySelector('.faq-answer');
   const arrowIcon = faq.querySelector('.icon');
   if (arrowIcon === e.target) {
     faq.classList.toggle('active');
+    if (answer.style.maxHeight === '0px' || answer.style.maxHeight === '') {
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    } else {
+      answer.style.maxHeight = '0';
+    }
   } else {
     closeAllOtherFaqs(faq);
     faq.classList.toggle('active');
+    if (answer.style.maxHeight === '0px' || answer.style.maxHeight === '') {
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    } else {
+      answer.style.maxHeight = '0';
+    }
   }
 }
 
